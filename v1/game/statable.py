66750576cec882431bcc44d06f4c0a6629983922
dsomorov <dsomorov@gmail.com>
2023-09-19 21:51:37 -0500

    move adding a listener to the Stat class

--- v1/game/statable.py
+++ v1/game/statable.py
@@ -6,6 +6,14 @@ class Statable(Subject):
         super().__init__()
         self.stats = {}
 
+    def add_stat(self, stat_type, base, current=None):
+        stat = Stat(stat_type, base, current=current)
+        self.stats[stat_type] = stat
+        stat.add_listener(
+            "on_stat_update",
+            lambda event_data: self.trigger_event("on_stat_update", event_data),
+        )
+
     def has_stat(self, stat_type):
         return stat_type in self.stats
 
@@ -17,18 +25,10 @@ class Statable(Subject):
 
     def add_stat_source(self, stat_type, source):
         if not stat_type in self.stats:
-            self.stats[stat_type] = Stat(stat_type, 0)
+            self.add_stat(stat_type, 0)
 
         stat = self.stats[stat_type]
         stat.add_source(source)
-
-        def _update_func(event_data):
-            if event_data["stat"] != stat_type:
-                return
-
-            stat.update_source(source, event_data["amount"])
-
-        source.add_listener("on_stat_update", _update_func)
 
 
 class Stat(Subject):
@@ -42,24 +42,24 @@ class Stat(Subject):
 
     def adjust_current(self, amount):
         self.current += amount
-        self._trigger_update()
+        self._trigger_update(amount)
 
     def add_source(self, source):
         self.sources[source] = 0
 
+        def _update_func(event_data):
+            if event_data["stat"] != self.stat_type:
+                return
+            self.update_source(source, event_data["amount"])
+
+        source.add_listener("on_stat_update", _update_func)
+
     def update_source(self, source, amount):
-        diff = amount - self.sources[source]
-        self.mod += diff
-        self.current += diff
-        self.sources[source] = amount
-        self._trigger_update()
+        self.mod += amount
+        self.current += amount
+        self.sources[source] += amount
+        self._trigger_update(amount)
 
-    def _trigger_update(self):
-        stat_update_event_data = {"stat": self.stat_type, "amount": self.current}
+    def _trigger_update(self, amount):
+        stat_update_event_data = {"stat": self.stat_type, "amount": amount}
         self.trigger_event("on_stat_update", stat_update_event_data)
-
-
-class StatModifier(Statable):
-    def __init__(self, stat_type, amount):
-        super().__init__()
-        self.stats[stat_type]
