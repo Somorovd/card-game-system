4938b02892ee4a9dc9e86d8e0fea5900b5edbf43
dsomorov <dsomorov@gmail.com>
2023-09-19 21:46:54 -0500

    move player fixture to init

--- v1/test/__init__.py
+++ v1/test/__init__.py
@@ -5,9 +5,9 @@ from game import *
 @pytest.fixture
 def players():
     jay = Player("Jay")
-    jay.stats["health"] = Stat("health", 10)
-    jay.stats["max_health"] = Stat("max_health", 100)
+    jay.add_stat("health", 10)
+    jay.add_stat("max_health", 100)
     larry = Player("Larry")
-    larry.stats["health"] = Stat("health", 10)
-    larry.stats["max_health"] = Stat("max_health", 100)
+    larry.add_stat("health", 10)
+    larry.add_stat("max_health", 100)
     return {"jay": jay, "larry": larry}
