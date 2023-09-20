3279609e5bf6efaaea553bd61f8682a9093606a9
dsomorov <dsomorov@gmail.com>
2023-09-19 21:51:53 -0500

    add player counter targeter

--- v1/game/targeters.py
+++ v1/game/targeters.py
@@ -36,7 +36,21 @@ class AttachedRelicTargeter(Targeter):
         return [effect.relic]
 
 
+class AttachedPlayerCounterTargeter(Targeter):
+    def get_targets(self, effect, event_data):
+        from .effects import Counter
+
+        relics = effect.relic.player.relics
+        counters = []
+        for relic in relics:
+            for effect in relic.effects:
+                if isinstance(effect, Counter):
+                    counters.append(effect)
+        return counters
+
+
 event_data_targeter = EventDataTargeter()
 attached_player_targeter = AttachedPlayerTargeter()
 attached_player_relics_targeter = AttachedPlayerRelicsTargeter()
 attached_relic_targeter = AttachedRelicTargeter()
+attached_player_counter_targeter = AttachedPlayerCounterTargeter()
