b70d2b97509c2ad873a4d0c3480b44598a5c4c3e
dsomorov <dsomorov@gmail.com>
2023-09-19 21:50:04 -0500

    remove print

--- v1/game/player.py
+++ v1/game/player.py
@@ -22,24 +22,17 @@ class Player(Statable):
         self.stats["health"].current = min(self.stats["max_health"].current, val)
 
     def add_relic(self, relic):
-        print(f"{self.name} is equipping {relic.name}")
         self.relics.append(relic)
         relic.equip_to_player(self)
-        print()
         add_relic_event_data = {"player": self, "relic": relic}
         res = GAME_MANAGER.trigger_event("on_player_add_relic", add_relic_event_data)
 
     def attack(self, target, amount):
-        print(f"Pre Attack: {self.name} is about to attack {target.name} for {amount}")
         pre_attack_event_data = {"player": self, "damage": amount, "target": target}
         res = GAME_MANAGER.trigger_event("on_player_pre_attack", pre_attack_event_data)
 
-        print(
-            f"Attack: {self.name} is attacks {res['target'].name} ({res['target'].health} HP) for {res['damage']}"
-        )
         res["target"].take_damage(self, res["damage"])
 
-        print(f"Post Attack: {res['target'].name} has {res['target'].health}\n")
         post_attack_event_data = res
         res = GAME_MANAGER.trigger_event(
             "on_player_post_attack", post_attack_event_data
@@ -54,7 +47,6 @@ class Player(Statable):
         GAME_MANAGER.trigger_event("on_player_post_heal", post_heal_event_data)
 
     def take_damage(self, source, amount):
-        print(f"Pre Take Damage: {self.name} is about to take {amount} damage")
         pre_take_damage_event_data = {
             "player": self,
             "damage": amount,
@@ -64,13 +56,11 @@ class Player(Statable):
             "on_player_pre_take_damage", pre_take_damage_event_data
         )
 
-        print(f"Take Damage: {self.name} takes {res['damage']} damage")
         self.health -= res["damage"]
 
         # if the post_take_damage event happens here, now, it will happen before the
         # attacker's post_attack event
 
-        print(f"Post Take Damage: {self.name} has {self.health} HP")
         post_take_damage_event_data = res
         GAME_MANAGER.trigger_event(
             "on_player_post_take_damage", post_take_damage_event_data
