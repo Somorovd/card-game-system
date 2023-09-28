528f538881d996a24986b36916ffb4ddb9cd2f8a
somorovd <dsomorov@gmail.com>
2023-09-28 15:12:58 -0400

    update player stat initialization

--- slay_the_spire/game/player.py
+++ slay_the_spire/game/player.py
@@ -6,18 +6,19 @@ class Player(Statable):
     def __init__(self, name):
         super().__init__()
         self.name = name
-        self.stats = {
-            "health": Stat("health", 100),
-            "max_health": Stat("max_health", 100),
-        }
         self._event_manager = EventManager()
 #         self.relics = []
+        self.init_stats()
 
 #     def add_relic(self, relic):
 #         self.relics.append(relic)
 #         relic.equip_to_player(self)
 #         add_relic_event_data = {"player": self, "relic": relic}
 #         res = EVENT_MANAGER.trigger_event("on_player_add_relic", add_relic_event_data)
+
+    def init_stats(self):
+        self.add_stat("health", 100)
+        self.add_stat("max_health", 100)
 
     def attack(self, target, amount):
         pre_attack_event_data = {"player": self, "amount": amount, "target": target}
