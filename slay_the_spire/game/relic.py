964ab259099d6956c7e18a513de23fb69abac68a
somorovd <dsomorov@gmail.com>
2023-09-28 22:10:29 -0400

    remove unused code

--- slay_the_spire/game/relic.py
+++ slay_the_spire/game/relic.py
@@ -1,8 +1,4 @@
-# from .event_manager import EVENT_MANAGER
 from .statable import Statable, Stat
-# from .effects import *
-# from .validators import *
-# from .targeters import *
 
 
 class Relic(Statable):
@@ -18,9 +14,3 @@ class Relic(Statable):
     def on_equip(self):
         for effect in self.effects:
             effect.arm_trigger(True)
-
-#     def equip_to_player(self, player):
-#         self.player = player
-#         for effect in self.effects:
-#             EVENT_MANAGER.add_listener(effect.event_name, effect.update)
-#             effect.on_equip(player)
