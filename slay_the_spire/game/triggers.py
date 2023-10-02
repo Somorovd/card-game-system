ffbc1126d6d3e1d37140b9b3056207421b3ea2b0
somorovd <dsomorov@gmail.com>
2023-10-01 22:22:53 -0400

    add an OnEquip trigger

--- slay_the_spire/game/triggers.py
+++ slay_the_spire/game/triggers.py
@@ -5,3 +5,16 @@ from .validators import *
 PlayerTookDamageTrigger = lambda: EventTrigger(
     "on_player_post_take_damage", PropertyInRange("amount", min=1)
 )
+
+
+class OnEquipTrigger(Toggle):
+    def __init__(self):
+        super().__init__()
+        self.set_init_toggled(True)
+        self.set_trigger(EventTrigger("on_player_equip_relic"))
+
+    def update(self, event_data, trigger=None):
+        print("here")
+        super().update(event_data, trigger=trigger)
+        if trigger == self._trigger:
+            self.disarm()
